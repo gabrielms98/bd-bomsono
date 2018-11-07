a559c37d61ea4cf3f95682fa200caf41f4d340b4
Gabriel M Silva <gabriel.silva@nobugs.com.br>
2018-11-07 12:35:28 -0200

    sequelize na veia

---
+++ src/renderer/backend.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+const remote = require('electron').remote;
+
+//helper functions
+const fatal_error = err => {remote.dialog.showErrorBox('Não foi possível conectar ao banco de dados!', err); remote.getCurrentWindow().close();}
+
+//global vars/objects
+var models;
+
+const sequelize = new Sequelize('sqlite:laborrural.db');
+
+const sync = () => sequelize.sync();
+const loadmodels = () => {
+  models = require('./models')(sequelize, Sequelize);
+  sync();
+}
+
+sequelize.authenticate().then(loadmodels, err => fatal_error(err));
+
+//main object
+const backend = {
+  install: (Vue, options) => {
+    Vue.prototype.$backend = {
+
+      synced: false,
+
+
+    }
+  }
+}
