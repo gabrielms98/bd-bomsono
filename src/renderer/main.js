b4b9e44aa8dea9b41b2025d09bbcb9c284196dad
gabrielms98 <gmbiel1@gmail.com>
2018-11-06 23:34:21 -0200

    botao e teste com session

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -3,14 +3,18 @@ import axios from 'axios'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import VueSession from 'vue-session'
+
 
 import App from './App'
 import router from './router'
 import store from './store'
 
+Vue.use(VueSession)
 Vue.use(Vuetify, {
-
+  iconfont: 'mdi'
 })
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
